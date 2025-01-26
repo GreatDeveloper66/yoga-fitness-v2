90a1e3e88d372bb4e5481256f8e530ec5c4d43f5
GreatDeveloper66 <ashaffe@gmail.com>
2025-01-25 20:30:44 -0600

    build front end react app

--- server/fallback.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-export function saveToFile(data) {
-  const filePath = "./fallback-data.txt";
-  fs.appendFile(filePath, JSON.stringify(data) + "\n", (err) => {
-    if (err) console.error("Failed to write to file:", err);
-    else console.log("Data saved to fallback file.");
-  });
-}
